9b28dfb48c75b4c3354d003fa2828bb76065da9a
mrinmoy30 <90055135+mrinmoy30@users.noreply.github.com>
2023-06-30 22:35:38 +0530

    Add files via upload

---
+++ L2.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+/*
+  int a = 123;
+
+  cout << a << '\n'; //also <<endl;
+
+  char b = 'v';
+  cout << b <<endl;
+
+  bool bl = true; //true will print 1 and flase will print 0
+  cout << bl <<endl;
+
+ float f = 1.2;
+ cout << f << endl;
+
+ double d = 1.23;
+ cout << d <<endl;
+
+ int size = sizeof(a); //(d) or (b) etc
+ cout << "size of a is:" << size <<endl;
+
+
+ //Typecasting
+ int e = 'e';
+ cout << e <<endl;
+
+ char ch = 98;
+ cout << ch <<endl;
+
+ char ch1 = 123456;
+ cout << ch1 <<endl; //not possible
+
+ //store only positive nos
+
+ unsigned int a =  112;
+ cout << a <<endl;
+
+ int a = 2.0/5;
+ cout << 2.0/5 <<endl;
+
+int a = 2;
+int b = 3;
+bool first = (a==b);
+cout << first << endl;
+bool second = (a>b);
+cout << second << endl;
+
+bool third = (a<b);
+cout << third << endl;
+
+bool fourth = (a<=b);
+cout << fourth << endl;
+
+bool fifth = (a>=b);
+cout << fifth << endl;
+
+bool sixth = (a!=b);  //not equal to sign
+cout << sixth << endl;
+*/
+
+int a = 21;
+cout << !a << endl;
+}
